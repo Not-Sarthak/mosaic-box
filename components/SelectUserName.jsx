511717398f3ae442210ade4706f51c671a6d3c1a
Sarthak <notsarthakshah@gmail.com>
2024-02-16 11:17:40 +0530

    select user name basic page for integration

---
+++ components/SelectUserName.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SelectUserName = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Select your name" />
+    </div>
+  )
+}
+
+export default SelectUserName
